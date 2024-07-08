3464a3942ddb53b84a65b3d47681a41b945f7ec2
CalAnders <hello@example.com>
2024-07-08 16:31:31 -0400

    Added input listeners

--- src/main/java/com/calanders/calengine/engine/MouseListener.java
+++ src/main/java/com/calanders/calengine/engine/MouseListener.java
@@ -1,5 +1,8 @@
 package com.calanders.calengine.engine;
 
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
 public class MouseListener {
     private static MouseListener instance;
     private double scrollX;
@@ -20,15 +23,37 @@ public class MouseListener {
         this.lastY = 0.0;
     }
 
-    public static void mousePosCallback(long window, double xPos, double yPos) {
+    public static void cursorPosCallback(long window, double xPos, double yPos) {
         get().lastX = get().xPos;
         get().lastY = get().yPos;
         get().xPos = xPos;
         get().yPos = yPos;
+        get().isDragging = get().mouseButtonPressed[0]
+                || get().mouseButtonPressed[1]
+                || get().mouseButtonPressed[2];
     }
 
     public static void mouseButtonCallback(long window, int button, int action, int mods) {
-        get().mouseButtonPressed[button] = true;
+        if (button < get().mouseButtonPressed.length) {
+            if (action == GLFW_PRESS) {
+                get().mouseButtonPressed[button] = true;
+            } else if (action == GLFW_RELEASE) {
+                get().mouseButtonPressed[button] = false;
+                get().isDragging = false;
+            }
+        }
+    }
+
+    public static void scrollCallback(long window, double xOffset, double yOffset) {
+        get().scrollX = xOffset;
+        get().scrollY = yOffset;
+    }
+
+    public static void endFrame() {
+        get().scrollX = 0;
+        get().scrollY = 0;
+        get().lastX = get().xPos;
+        get().lastY = get().yPos;
     }
 
     public static MouseListener get() {
@@ -36,5 +61,37 @@ public class MouseListener {
             instance = new MouseListener();
         }
         return instance;
+    }
+
+    public static float getX() {
+        return (float) get().xPos;
+    }
+
+    public static float getY() {
+        return (float) get().yPos;
+    }
+
+    public static float getDx() {
+        return (float) (get().lastX - get().xPos);
+    }
+
+    public static float getDy() {
+        return (float) (get().lastY - get().yPos);
+    }
+
+    public static float getScrollX() {
+        return (float) get().scrollX;
+    }
+
+    public static float getScrollY() {
+        return (float) get().scrollY;
+    }
+
+    public static boolean isDragging() {
+        return get().isDragging;
+    }
+
+    public static boolean mouseButtonDown(int button) {
+        return button < get().mouseButtonPressed.length && get().mouseButtonPressed[button];
     }
 }
